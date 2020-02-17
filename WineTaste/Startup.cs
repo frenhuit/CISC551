2fefc9764f3de3766b82ba525ee01f9db84d003e
frenhuit <frenhuit@gmail.com>
2020-02-16 21:38:45 -0800

    Use CDN rather than local file for bootstrap.

--- WineTaste/Startup.cs
+++ WineTaste/Startup.cs
@@ -31,6 +31,8 @@ namespace WineTaste
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseStaticFiles(); 
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
